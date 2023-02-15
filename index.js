7633fb1b2b03cb185fb254fe9563d688f708b36a
Nazutte <privacy9136@gmail.com>
2023-02-15 12:39:31 +0500

    cpr progress 85% completed

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const ExcelJS = require('exceljs')
-const cashflow = require('./resources/cashflow.json');
-const { boldCenter } = require('./styles')
+const cashflow = require('./resources/cashflow.json')
+const { startCase } = require('lodash')
+const { boldCenter, verticalBold, right, bold, center } = require('./styles')
 
 let worksheet;
 let rowCount;
@@ -8,6 +9,7 @@ let allTotalIndex;
 let amountOfColumns;
 let isFirstHalf;
 let colSpan;
+let span;
 let dayStart;
 
 async function main(){
@@ -41,7 +43,7 @@ main();
 
 // FUNCTIONS
 function createCashflowExcel(year, month, tableName){
-  rowCount = 5;
+  rowCount = 6;
 
   worksheet.getColumn('A').width = 4;
   worksheet.getColumn('B').width = 15;
@@ -59,7 +61,9 @@ function createCashflowExcel(year, month, tableName){
     colSpan = amountOfColumns + 3
     dayStart = 16;
   }
+  span = String.fromCharCode(65 + colSpan);
 
+  // PAGE SETUP
   let details = ['Details'];
   const days = Array.from({length: amountOfColumns}, (_, i) => i + dayStart);
   details = details.concat(days);
@@ -70,11 +74,24 @@ function createCashflowExcel(year, month, tableName){
   }
 
   worksheet.mergeCells(`A${rowCount}:B${rowCount}`);
-  insertRow(details, boldCenter);
+  insertRow(details, boldCenter, boldCenter, false);
   rowCount++;
 
-  const diff = ['securityDeposit'];
-  insertType('cashIn', allTotalIndex, diff);
+  // CASH IN
+  worksheet.mergeCells(`A${rowCount}:${span}${rowCount}`);
+  Object.assign(worksheet.getCell(`A${rowCount}`), bold, { value: '  CASH IN' });
+  rowCount++;
+
+  const cashInDiff = ['securityDeposit'];
+  insertType('cashIn', allTotalIndex, cashInDiff);
+
+  // CASH OUT
+  worksheet.mergeCells(`A${rowCount}:${span}${rowCount}`);
+  Object.assign(worksheet.getCell(`A${rowCount}`), bold, { value: '  CASH OUT' });
+  rowCount++;
+
+  const cashOutDiff = ['cashOutOther'];
+  insertType('cashOut', allTotalIndex, cashOutDiff);
 }
 
 function insertType(cashflowTypeString, allTotalIndex, diff){
@@ -86,30 +103,48 @@ function insertType(cashflowTypeString, allTotalIndex, diff){
 
     if(!found){
       for(const category in cashflowType[type]){
+        const categoryTotal = cashflow.bothHalfTotal[cashflowTypeString][type][category][allTotalIndex];
         let arr = [category];
         arr = arr.concat(cashflowType[type][category]);
-        insertRow(arr, boldCenter);
+        arr.push(categoryTotal);
+        insertRow(arr, center, right, false);
         rowCount++;
         colLength++;
       }
-    }
-    
-    // console.log(type);
-    // console.log('Column Length: ' + colLength);
-    // console.log(rowCount - colLength);
-    // console.log(rowCount - 1);
-    // console.log('\n');
 
-    if(!found){
+      const typeCell = worksheet.getCell(`A${rowCount - colLength}`);
       if((rowCount - 1) != (rowCount - colLength)){
         worksheet.mergeCells(`A${rowCount - colLength}:A${rowCount - 1}`);
       }
+      Object.assign(typeCell, verticalBold, { value: startCase(type) });
+
+      const typeTotal = cashflow.cashflowObj[allTotalIndex].allTotal[type];
+      const typeTotalsTotal = cashflow.bothHalfTotal.allTotal[type][allTotalIndex];
+      const arr = ['', 'Total'].concat(typeTotal, typeTotalsTotal);
+      insertRow(arr, boldCenter, right, true);
+      rowCount++
     }
   }
+
+  diff.forEach(type => {
+    for(const category in cashflowType[type]){
+      const categoryTotal = cashflow.bothHalfTotal[cashflowTypeString][type][category][allTotalIndex];
+      let arr = ['', category];
+      arr = arr.concat(cashflowType[type][category]);
+      arr.push(categoryTotal);
+
+      insertRow(arr, center, right, true);
+      rowCount++;
+    }
+  });
 }
 
-function insertRow(values, style){
+function insertRow(values, detailStyle, valueStyle, diff){
   let columnCount = 66;
+
+  if(diff == true){
+    columnCount -= 1
+  }
 
   values.forEach(value => {
     if(typeof value == 'object'){
@@ -119,10 +154,24 @@ function insertRow(values, style){
         value = value.amount;
       }
     }
+
+    if(typeof value == 'string'){
+      value = startCase(value);
+    }
+
+    if(typeof value == 'number' && values[0] != 'Details'){
+      value = (value / 100).toFixed(2);
+      value += ' ';
+    }
+
     const cellName = String.fromCharCode(columnCount) + rowCount;
     const cell = worksheet.getCell(cellName);
 
-    Object.assign(cell, style, { value });
+    if(columnCount == 66){
+      Object.assign(cell, detailStyle, { value });
+    } else {
+      Object.assign(cell, valueStyle, { value });
+    }
     columnCount++;
   });
 }
--- styles.js
+++ styles.js
@@ -1,15 +1,93 @@
+const size = 7;
+const border = {
+  top: {style:'thin'},
+  left: {style:'thin'},
+  bottom: {style:'thin'},
+  right: {style:'thin'},
+}
+const fontName = 'DejaVu Sans';
+
 const boldCenter = {
   font: {
     bold: true,
-    name: 'Calibri',
-    size: 10,
+    name: fontName,
+    size,
   },
   alignment: {
     horizontal: 'center', 
     vertical: 'middle',
-  }
+  },
+  border,
+}
+
+const verticalBold = {
+  font: {
+    bold: true,
+    name: fontName,
+    size,
+  },
+  alignment: {
+    horizontal: 'center', 
+    vertical: 'middle',
+    textRotation: 90,
+    wrapText: true,
+  },
+  border,
+}
+
+const right = {
+  font: {
+    name: fontName,
+    size,
+  },
+  alignment: {
+    horizontal: 'right', 
+    vertical: 'middle',
+  },
+  border,
+}
+
+const rightBold = {
+  font: {
+    bold: true,
+    name: fontName,
+    size,
+  },
+  alignment: {
+    horizontal: 'right', 
+    vertical: 'middle',
+  },
+  border,
+}
+
+const bold = {
+  font: {
+    bold: true,
+    name: fontName,
+    size,
+  },
+  alignment: {
+    vertical: 'middle',
+  },
+  border,
+}
+
+const center = {
+  font: {
+    name: fontName,
+    size,
+  },
+  alignment: {
+    horizontal: 'center', 
+    vertical: 'middle',
+  },
+  border,
 }
 
 module.exports = {
   boldCenter,
+  verticalBold,
+  right,
+  bold,
+  center,
 }
